a472d90552625c91322a669fc7c29f4f02f779ab
HEN <eogie@naver.com>
2022-06-15 19:53:54 +0900

    initial commit

---
+++ src/main/java/org/eogie/example/ExternalProgram.java
@@ -0,0 +1,7 @@
+package org.eogie.example;
+
+public class ExternalProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/eogie/example/JettyServer.java
@@ -0,0 +1,4 @@
+package org.eogie.example;
+
+public class JettyServer {
+}
