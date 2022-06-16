628de1164075699b8041c33dfd0be322b2a5d9a1
HEN <eogie@naver.com>
2022-06-16 17:12:20 +0900

    console example

---
+++ src/main/java/org/eogie/example/ConsoleExample.java
@@ -0,0 +1,26 @@
+package org.eogie.example;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ConsoleExample {
+
+    public static void main(String[] args) {
+        new ConsoleExample().start();
+    }
+
+    public void start() {
+        try (Scanner in = new Scanner(System.in);
+                FileWriter fileWriter = new FileWriter("test/report.txt", true);
+                PrintWriter out = new PrintWriter(fileWriter, true)) {
+            String line;
+            while ((line = in.nextLine()) != null) {
+                out.println("reply = " + line);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
